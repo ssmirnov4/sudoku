32e4819b4ec2ae4ccef5b911f904fc5f67789e9e
ssmirnov4 <yaroslav-763@mail.ru>
2024-11-22 22:40:36 +0600

    Version 0.0.2 / Added sudoku generation

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,7 @@
 import pygame
 import settings as s
 import random
+import numpy as np
 
 pygame.init()
 
@@ -9,7 +10,7 @@ pygame.display.set_caption("Судоку")
 
 def drawGrid():
    for i in range(10):
-    if(i % 3 == 0):
+    if i % 3 == 0:
         lineThickness = 3
     else:
         lineThickness = 1
@@ -17,11 +18,40 @@ def drawGrid():
     pygame.draw.line(screen, s.black, (i*s.cellSize, 0), (i*s.cellSize, s.width), lineThickness)
 
 
-def is_valid(table, row, col, num):
+def valid(table, row, col, num):
     if num in table[row]:
         return False
+
     if num in [table[i][col] for i in range(9)]:
         return False
 
+    startRow = 3 * (row//3)
+    startCol = 3 * (col//3)
+    for i in range(3):
+        for j in range(3):
+            if table[startRow + i][startCol+j] == num:
+                return False
+
+    return True
+
+def fill(table):
+    for row in range(9):
+        for col in range(9):
+            if table[row][col] == 0:
+                for number in random.sample((1,9), 9):
+                    if valid(table, row, col, number):
+                        table[row][col] = number
+                        if fill(table):
+                            return True
+                        table[row][col] = 0
+                return False
+    return True
 
 
+def generate():
+    table = np.zeros((9,9), dtype=int)
+    fill(table)
+    return table
+
+def remove(table, numCells):
+    return True
